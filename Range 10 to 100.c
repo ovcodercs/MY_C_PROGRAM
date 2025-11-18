774dab1df6e9772ff15afb96e68650b3136f9ca7
ovcodercs <vaishnavirathodrathod902@gmail.com>
2025-11-18 23:11:03 +0530

    Create Range 10 to 100.c

---
+++ Range 10 to 100.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter a number");
+    scanf("%d",num);
+    if(num>=10 && num<=100);
+    printf("in range");
+    else
+    printf("not in range");
+}
